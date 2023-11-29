d768e464e30612b13253aa33c62f96c5a4e0f909
Andreas Selguson <github@selguson.se>
2023-11-29 15:46:10 +0100

    Connected activitys with eachother

--- app/src/main/java/com/example/ma23_android_project_1/GameActivity.kt
+++ app/src/main/java/com/example/ma23_android_project_1/GameActivity.kt
@@ -2,10 +2,35 @@ package com.example.ma23_android_project_1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 
 class GameActivity : AppCompatActivity() {
+
+    lateinit var pointsView: TextView
+    lateinit var cardImage : ImageView
+
+    val points : Int = 0;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
+
+        pointsView = findViewById(R.id.pointTextView)
+        var name = ""
+        name = intent.getStringExtra("player").toString()
+        Log.d("!!!", name)
+        pointsView.text = "$name: $points poäng"
+
+        val higherButton = findViewById<Button>(R.id.higherButton)
+        val lowerButton = findViewById<Button>(R.id.lowerButton)
+        val evenButton = findViewById<Button>(R.id.evenButton)
+
+        higherButton.setOnClickListener {
+            finish()
+        }
+
     }
 }
--- app/src/main/java/com/example/ma23_android_project_1/MainActivity.kt
+++ app/src/main/java/com/example/ma23_android_project_1/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.example.ma23_android_project_1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var nameText : EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        nameText = findViewById(R.id.nameEditText)
+
+        val button = findViewById<Button>(R.id.continueButton)
+        button.setOnClickListener {
+            letsContinue()
+        }
+
+    }
+    fun letsContinue(){
+        val intent = Intent(this, GameActivity::class.java)
+        var name = nameText.text
+        if(name.equals("")){
+            //name = ""
+        }
+        Log.d("!!!", nameText.text.toString())
+        Log.d("!!!", name.toString())
+        intent.putExtra("player", name)
+        startActivity(intent)
     }
 }
